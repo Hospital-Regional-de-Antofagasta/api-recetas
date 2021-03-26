8b0df6bcf85b34e1dd3fdf4b4b6945c2b634dca3
Claudio González Salazar <pyrates_claudio@hotmail.com>
2021-03-26 13:35:36 -0300

    HRAP-23 ADD: Se añaden try-catch en los endpoints.

--- api/routes/recetas.js
+++ api/routes/recetas.js
@@ -5,43 +5,45 @@ const estaAutenticado = require('../middleware/auth')
 const router = express.Router()
 
 router.get('/recetas_paciente/:numero',estaAutenticado, async (req, res) =>{
-    console.log(req.params.numero)
-        
-    await Recetas.find({
-        PAC_PAC_Numero: req.params.numero
-    })    
-    .exec()
-    .then(x => res.status(200).send(x))
-       
+    try{
+        await Recetas.find({
+            PAC_PAC_Numero: req.params.numero
+        })    
+        .exec()
+        .then(x => res.status(200).send(x))
+    }catch(error){
+        res.status(500).send({ respuesta:'Se produjo un error.' })
+    }    
 })
 
 router.get('/detalles_receta/:numero&:tipo',estaAutenticado, async(req, res) =>{
-    console.log(req.params.numero)
-    console.log(req.params.tipo)
-    Promise.all([
-        await Recetas.findOne({
-            Fld_NroRecetaOriginal:req.params.numero,
-            Fld_TipoRecetOriginal:req.params.tipo
-        }).exec(),
-        await RecetasDetalles.findOne({
-            Fld_NroRecetaOriginal:req.params.numero,
-            Fld_TipoRecetOriginal:req.params.tipo
-        }).exec()           
-    ])
-    .then(x => {
-        const algo ={
-            Fld_NroRecetaMadre: x[0]['Fld_NroRecetaOriginal'],
-            Fld_TipoRecet: x[0]['Fld_TipoRecetOriginal'],
-            Fld_FechaDigit:x[0]['Fld_FechaDigit'],            
-            Fld_MedicoPrescriptor:x[0]['Fld_MedicoPrescriptor'],
-            PAC_PAC_Numero: x[0]['PAC_PAC_Numero'],
-            Fld_PatologiaCronica: x[0]['Fld_PatologiaCronica'],
-            Pases:x[0]['Pases'],
-            Medicamentos:x[1]['Medicamentos']           
-        }
-        console.log(algo)
-        res.status(200).send(algo)
-    })
+    try{
+        Promise.all([
+            await Recetas.findOne({
+                Fld_NroRecetaOriginal:req.params.numero,
+                Fld_TipoRecetOriginal:req.params.tipo
+            }).exec(),
+            await RecetasDetalles.findOne({
+                Fld_NroRecetaOriginal:req.params.numero,
+                Fld_TipoRecetOriginal:req.params.tipo
+            }).exec()           
+        ])
+        .then(x => {
+            const recetaDetallada ={
+                Fld_NroRecetaMadre: x[0]['Fld_NroRecetaOriginal'],
+                Fld_TipoRecet: x[0]['Fld_TipoRecetOriginal'],
+                Fld_FechaDigit:x[0]['Fld_FechaDigit'],            
+                Fld_MedicoPrescriptor:x[0]['Fld_MedicoPrescriptor'],
+                PAC_PAC_Numero: x[0]['PAC_PAC_Numero'],
+                Fld_PatologiaCronica: x[0]['Fld_PatologiaCronica'],
+                Pases:x[0]['Pases'],
+                Medicamentos:x[1]['Medicamentos']           
+            }
+            res.status(200).send(recetaDetallada)
+        })
+    }catch(error){
+        res.status(500).send({ respuesta:'Se produjo un error.' })
+    }  
 })
 
 
