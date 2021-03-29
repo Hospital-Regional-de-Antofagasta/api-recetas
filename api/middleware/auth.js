14addf827b672796727e4ca717eb21b717df8d4f
Claudio González Salazar <pyrates_claudio@hotmail.com>
2021-03-29 09:42:46 -0300

    HRAP-23 CHANGE: Se añade manejo de error cuando expira el token. Se cambian los catch y se quitan los await de los endpoints.

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,17 +1,20 @@
 const jwt = require('jsonwebtoken')
+const secreto = 'mi-secreto'
 
 const estaAutenticado = (req, res, next) => {
     const token = req.headers.authorization
     if(!token){
         return res.sendStatus(403)
     }
-    jwt.verify(token, 'mi-secreto', (err, decoded) => {
+    jwt.verify(token, secreto, (error, decoded) => {
+        if (error) {
+            return res.sendStatus(403)
+            //return res.status(403).send(error)
+        }
         const {_id} =  decoded
         req.idPaciente = _id
         next()
     })
 }
-
-
 
 module.exports = estaAutenticado
