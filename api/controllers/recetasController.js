3cf0a692ee105ca2824137b8989f806606eecb0d
Pablo Fontecilla <pafonbu@gmail.com>
2021-08-03 16:23:27 -0400

    ADD: enviar detalles errores

--- api/controllers/recetasController.js
+++ api/controllers/recetasController.js
@@ -8,6 +8,14 @@ exports.getRecetasPaciente = async (req, res) => {
     }).exec();
     res.status(200).send(arregloRecetas);
   } catch (error) {
+    if (process.env.NODE_ENV === "dev")
+      return res.status(500).send({
+        respuesta: await getMensajes("serverError"),
+        detalles_error: {
+          nombre: error.name,
+          mensaje: error.message,
+        },
+      });
     res.status(500).send({ respuesta: await getMensajes("serverError") });
   }
 };
