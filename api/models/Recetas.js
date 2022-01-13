6116d0281f04d7087bb8962a7a2265c231d1d5ea
Pablo Fontecilla <pafonbu@gmail.com>
2022-01-13 11:11:31 -0300

    CHANGE: modificaciones para nuevo modelo

--- api/models/Recetas.js
+++ api/models/Recetas.js
@@ -2,27 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema(
-  {
-    numeroRecetaOriginal: Number,
-    tipoRecetaOriginal: Number,
-    medicoPrescriptor: String,
-    numeroPaciente: { type: Number, select: false },
-    patologiaCronica: String,
-    recetaRetenida: Boolean,
-    pases: [
-      {
-        numeroReceta: Number,
-        fechaEmision: Date,
-        numeroPase: Number,
-      },
-    ],
-    medicamentos: [
-      {
-        nombreMaterial: String,
-        medicamentoControlado: Boolean,
-        mensaje: String,
-      },
-    ],
+  {correlativo: { type: Number, require: true },
+  numeroReceta: { type: Number, require: true },
+  tipoReceta: { type: Number, require: true },
+  rutPaciente: { type: String, require: true, select: false },
+  medicoPrescriptor: { type: String, require: true },
+  patologia: String,
+  recetaRetenida: { type: Boolean, default: false },
+  fechaEmision: { type: String, require: true },
+  codigoEstablecimiento: { type: String, require: true },
+  nombreEstablecimiento: { type: String, require: true },
+  pases: [
+    {
+      numeroReceta: { type: Number, require: true },
+      fechaEmisionFutura: { type: String, require: true },
+      numeroPase: { type: Number, require: true },
+    },
+  ],
+  medicamentos: { type: [
+    {
+      nombreMedicamento: { type: String, require: true },
+      medicamentoControlado: { type: Boolean, default: false },
+      mensaje: { type: String, require: true },
+    },
+  ], require: true }
   },
   { timestamps: true }
 );
--- tests/recetas.spec.js
+++ tests/recetas.spec.js
@@ -12,7 +12,7 @@ const configSeed = require("./testSeeds/configSeed.json");
 const secreto = process.env.JWT_SECRET;
 let token;
 
-beforeEach(async (done) => {
+beforeEach(async () => {
   //Cerrar la conexión que se crea en el index.
   await mongoose.disconnect();
   //Conectar a la base de datos de prueba.
@@ -23,21 +23,19 @@ beforeEach(async (done) => {
   //Cargar los seeds a la base de datos.
   await Recetas.create(recetasSeeds);
   await ConfigApiRecetas.create(configSeed);
-  done();
 });
 
-afterEach(async (done) => {
+afterEach(async () => {
   //Borrar el contenido de la colleccion en la base de datos despues de la pruebas.
   await Recetas.deleteMany();
   await ConfigApiRecetas.deleteMany();
   //Cerrar la conexión a la base de datos despues de la pruebas.
   await mongoose.connection.close();
-  done();
 });
 
 describe("Endpoints", () => {
   describe("/v1/recetas/recetas-paciente", () => {
-    it("Intenta obtener las recetas de un paciente sin token", async (done) => {
+    it("Intenta obtener las recetas de un paciente sin token", async () => {
       const respuesta = await request.get("/v1/recetas/recetas-paciente");
 
       const mensaje = await getMensajes("forbiddenAccess");
@@ -51,13 +49,12 @@ describe("Endpoints", () => {
           icono: mensaje.icono,
         },
       });
-      done();
     });
-    it("Intenta obtener las recetas de un paciente con token (Arreglo sin recetas)", async (done) => {
+    it("Intenta obtener las recetas de un paciente sin recetas", async () => {
       token = jwt.sign(
         {
           _id: "000000000000",
-          numeroPaciente:2,
+          rutPaciente: "22222222-2",
         },
         secreto
       );
@@ -69,13 +66,12 @@ describe("Endpoints", () => {
       const arregloRecetas = respuesta.body;
       expect(arregloRecetas).toStrictEqual([]);
       expect(arregloRecetas.length).toStrictEqual(0);
-      done();
     });
-    it("Intenta obtener las recetas de un paciente con token (Arreglo con recetas)", async (done) => {
+    it("Intenta obtener las recetas de un paciente con recetas", async () => {
       token = jwt.sign(
         {
           _id: "000000000000",
-          numeroPaciente: 1,
+          rutPaciente: "11111111-1",
         },
         secreto
       );
@@ -84,48 +80,105 @@ describe("Endpoints", () => {
         .set("Authorization", token);
       expect(respuesta.status).toBe(200);
       //Probar que el arreglo tiene dos recetas y que ambas son del mismo paciente.
-      const arregloRecetas = respuesta.body;
-      const primeraReceta = arregloRecetas[0];
-      const primerPase = primeraReceta.pases[0];
-      const segundoPase = primeraReceta.pases[1];
-      const medicamentosPrimeraReceta = primeraReceta.medicamentos;
-      const segundaReceta = arregloRecetas[1];
-      const tercerPase = segundaReceta.pases[0];
-      const medicamentosSegundaReceta = segundaReceta.medicamentos;
-      expect(arregloRecetas.length).toStrictEqual(2);
-      expect(primeraReceta.numeroPaciente).toBeFalsy();
-      expect(primeraReceta.numeroRecetaOriginal).toStrictEqual(24492986);
-      expect(primeraReceta.tipoRecetaOriginal).toStrictEqual(5);
-      expect(primeraReceta.recetaRetenida).toStrictEqual(false);
-      expect(primeraReceta.pases.length).toStrictEqual(2);
-      expect(primerPase.numeroReceta).toStrictEqual(24492990);
-      expect(primerPase.numeroPase).toStrictEqual(5);
-      expect(segundoPase.numeroReceta).toStrictEqual(24492991);
-      expect(segundoPase.numeroPase).toStrictEqual(6);
-      expect(medicamentosPrimeraReceta.length).toStrictEqual(1);
-      expect(medicamentosPrimeraReceta[0].nombreMaterial).toStrictEqual(
-        "PARACETAMOL CM 200 MG"
+      const recetas = respuesta.body;
+
+      expect(recetas.length).toBe(2)
+      // primera receta
+      expect(recetas[0].correlativo).toBe(1)
+      expect(recetas[0].numeroReceta).toBe(24492986)
+      expect(recetas[0].tipoReceta).toBe(5)
+      expect(recetas[0].rutPaciente).toBeFalsy()
+      expect(recetas[0].medicoPrescriptor).toBe("ALVARO PIZARRO GONZALEZ")
+      expect(recetas[0].patologia).toBe("Asma - EPOC")
+      expect(recetas[0].recetaRetenida).toBeFalsy()
+      expect(Date.parse(recetas[0].fechaEmision)).toBe(
+        Date.parse("2021-03-16T04:00:00.000Z")
       );
-      expect(medicamentosPrimeraReceta[0].medicamentoControlado).toStrictEqual(
-        true
+      expect(recetas[0].codigoEstablecimiento).toBe("HRA")
+      expect(recetas[0].nombreEstablecimiento).toBe("Hospital Regional Antofagasta Dr. Leonardo Guzmán")
+      // pases primera receta
+      // primer pase
+      expect(recetas[0].pases[0].numeroReceta).toBe(24492990)
+      expect(Date.parse(recetas[0].pases[0].fechaEmisionFutura)).toBe(
+        Date.parse("2021-04-16T04:00:00.000Z")
       );
-      expect(medicamentosPrimeraReceta[0].mensaje).toBe('2 cada 6 HRS. por 2 día(s)')
-      expect(segundaReceta.numeroPaciente).toBeFalsy();
-      expect(segundaReceta.numeroRecetaOriginal).toStrictEqual(25097726);
-      expect(segundaReceta.tipoRecetaOriginal).toStrictEqual(5);
-      expect(segundaReceta.recetaRetenida).toStrictEqual(true);
-      expect(segundaReceta.pases.length).toStrictEqual(1);
-      expect(tercerPase.numeroReceta).toStrictEqual(25097731);
-      expect(tercerPase.numeroPase).toStrictEqual(6);
-      expect(medicamentosSegundaReceta.length).toStrictEqual(1);
-      expect(medicamentosSegundaReceta[0].nombreMaterial).toStrictEqual(
-        "PARACETAMOL CM 500 MG"
+      expect(recetas[0].pases[0].numeroPase).toBe(1)
+      // segundo pase
+      expect(recetas[0].pases[1].numeroReceta).toBe(24492991)
+      expect(Date.parse(recetas[0].pases[1].fechaEmisionFutura)).toBe(
+        Date.parse("2021-05-16T04:00:00.000Z")
       );
-      expect(medicamentosSegundaReceta[0].medicamentoControlado).toStrictEqual(
-        false
+      expect(recetas[0].pases[1].numeroPase).toBe(2)
+      // tercer pase
+      expect(recetas[0].pases[2].numeroReceta).toBe(24492992)
+      expect(Date.parse(recetas[0].pases[2].fechaEmisionFutura)).toBe(
+        Date.parse("2021-06-16T04:00:00.000Z")
       );
-      expect(medicamentosSegundaReceta[0].mensaje).toBe('1 cada 12 HRS. por 3 día(s)')
-      done();
+      expect(recetas[0].pases[2].numeroPase).toBe(3)
+      // cuarto pase
+      expect(recetas[0].pases[3].numeroReceta).toBe(24492993)
+      expect(Date.parse(recetas[0].pases[3].fechaEmisionFutura)).toBe(
+        Date.parse("2021-07-16T04:00:00.000Z")
+      );
+      expect(recetas[0].pases[3].numeroPase).toBe(4)
+      // quinto pase
+      expect(recetas[0].pases[4].numeroReceta).toBe(24492994)
+      expect(Date.parse(recetas[0].pases[4].fechaEmisionFutura)).toBe(
+        Date.parse("2021-08-16T04:00:00.000Z")
+      );
+      expect(recetas[0].pases[4].numeroPase).toBe(5)
+      // medicamentos primera receta
+      expect(recetas[0].medicamentos[0].nombreMedicamento).toBe("PARACETAMOL CM 200 MG")
+      expect(recetas[0].medicamentos[0].medicamentoControlado).toBeTruthy()
+      expect(recetas[0].medicamentos[0].mensaje).toBe("2 cada 6 HRS. por 2 día(s)")
+      // segunda receta
+      expect(recetas[1].correlativo).toBe(2)
+      expect(recetas[1].numeroReceta).toBe(25097726)
+      expect(recetas[1].tipoReceta).toBe(5)
+      expect(recetas[1].rutPaciente).toBeFalsy()
+      expect(recetas[1].medicoPrescriptor).toBe("GABRIEL MARTINEZ FUENTES")
+      expect(recetas[1].patologia).toBe("Demencia y Trastornos Mentales Organicos")
+      expect(recetas[1].recetaRetenida).toBeTruthy()
+      expect(Date.parse(recetas[1].fechaEmision)).toBe(
+        Date.parse("2021-02-30T04:00:00.000Z")
+      );
+      expect(recetas[1].codigoEstablecimiento).toBe("HRA")
+      expect(recetas[1].nombreEstablecimiento).toBe("Hospital Regional Antofagasta Dr. Leonardo Guzmán")
+      // pases primera receta
+      // primer pase
+      expect(recetas[1].pases[0].numeroReceta).toBe(25097731)
+      expect(Date.parse(recetas[1].pases[0].fechaEmisionFutura)).toBe(
+        Date.parse("2021-03-30T03:00:00.000Z")
+      );
+      expect(recetas[1].pases[0].numeroPase).toBe(1)
+      // segundo pase
+      expect(recetas[1].pases[1].numeroReceta).toBe(25097732)
+      expect(Date.parse(recetas[1].pases[1].fechaEmisionFutura)).toBe(
+        Date.parse("2021-04-30T03:00:00.000Z")
+      );
+      expect(recetas[1].pases[1].numeroPase).toBe(2)
+      // tercer pase
+      expect(recetas[1].pases[2].numeroReceta).toBe(25097733)
+      expect(Date.parse(recetas[1].pases[2].fechaEmisionFutura)).toBe(
+        Date.parse("2021-05-30T03:00:00.000Z")
+      );
+      expect(recetas[1].pases[2].numeroPase).toBe(3)
+      // cuarto pase
+      expect(recetas[1].pases[3].numeroReceta).toBe(25097734)
+      expect(Date.parse(recetas[1].pases[3].fechaEmisionFutura)).toBe(
+        Date.parse("2021-06-30T03:00:00.000Z")
+      );
+      expect(recetas[1].pases[3].numeroPase).toBe(4)
+      // quinto pase
+      expect(recetas[1].pases[4].numeroReceta).toBe(25097735)
+      expect(Date.parse(recetas[1].pases[4].fechaEmisionFutura)).toBe(
+        Date.parse("2021-07-30T03:00:00.000Z")
+      );
+      expect(recetas[1].pases[4].numeroPase).toBe(5)
+      // medicamentos primera receta
+      expect(recetas[1].medicamentos[0].nombreMedicamento).toBe("PARACETAMOL CM 500 MG")
+      expect(recetas[1].medicamentos[0].medicamentoControlado).toBeFalsy()
+      expect(recetas[1].medicamentos[0].mensaje).toBe("1 cada 12 HRS. por 3 día(s)")
     });
   });
 });
